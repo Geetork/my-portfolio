d618c0ec191bfacb30c69e2f9ec592fd6e71aa83
geetork <galinaleespb@gmail.com>
2024-05-12 14:25:16 +0100

    loader changed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,8 +4,8 @@ import { Html } from '@react-three/drei';
 const Loader = () => {
   return (
     <Html>
-        <div className='flex justify-center items-center'>
-          <div className='w-20 h-20 border-4 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin'></div>
+        <div className="absolute top-0 left-0">
+            <svg className="animate-spin h-20 w-20 rounded-full bg-transparent border-4 border-transparent border-opacity-50" style={{borderRightColor: "#000000", borderTopColor: "#000000" }} viewBox="0 0 50 50"></svg>
         </div>
     </Html>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -53,7 +53,6 @@ const Home = () => {
       <div className="absolute top-20 left-0 right-0 z-10 flex items-center justify-center">
         { currentStage && <HomeInfo currentStage={currentStage}/>}
       </div>
-
       <Canvas 
         className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
         camera={{ near: 0.1, far: 1000 }}>
