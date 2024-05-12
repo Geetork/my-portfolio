35ae4ad616488d8e060087c43e28ffcf3cb8b834
geetork <galinaleespb@gmail.com>
2024-05-12 14:59:01 +0100

    loader changed

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,8 +3,9 @@ import { Html } from '@react-three/drei';
 
 const Loader = () => {
   return (
-    <Html>
-        <div className="absolute top-0 left-0">
+    <Html as='div'
+      className='absolute h-[100%] w-[100%] flex justify-center items-center'>
+        <div>
             <svg className="animate-spin h-20 w-20 rounded-full bg-transparent border-4 border-transparent border-opacity-50" style={{borderRightColor: "#000000", borderTopColor: "#000000" }} viewBox="0 0 50 50"></svg>
         </div>
     </Html>
